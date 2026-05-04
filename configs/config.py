9e565aaf5add15436efd2c8d29114e5af16eb29b
Denys <maherovskyidenys@gmail.com>
2026-05-04 14:56:03 -0400

    Added vars in config.py

---
+++ configs/config.py
@@ -0,0 +1,28 @@
+CLASSES = [
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+]
+
+IMG_SIZE = (448, 448)
+
+S = 7
+B = 2
+C = 20
