e57634d8a09845fa7770b81d768ce4cb6f177100
Denys <maherovskyidenys@gmail.com>
2026-05-06 12:10:11 -0400

    Remove old YOLO backbone and add pretrained ResNet18 backbone

--- src/models/model.py
+++ src/models/model.py
@@ -3,97 +3,24 @@ import torch.nn as nn
 
 from configs.config import C, B
 
-class YOLOv1(nn.Module):
+from torchvision.models import resnet18, ResNet18_Weights
+
+class YOLORes(nn.Module):
     def __init__(self, boxes: int = B, classes: int = C):
         super().__init__()
         self.b, self.c = boxes, classes
 
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.1),
-            nn.MaxPool2d(2, 2),
+        resnet = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        self.backbone = nn.Sequential(*(list(resnet.children())[:-2]))
 
-            nn.Conv2d(64, 192, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(192),
-            nn.LeakyReLU(0.1),
-            nn.MaxPool2d(2, 2),
+        self.head = nn.Sequential(
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1, stride=2, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1, inplace=True),
 
-            nn.Conv2d(192, 128, kernel_size=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 256, kernel_size=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1),
-            nn.MaxPool2d(2, 2),
-
-            nn.Conv2d(512, 256, kernel_size=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 256, kernel_size=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 256, kernel_size=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 256, kernel_size=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 512, kernel_size=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 1024, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.1),
-            nn.MaxPool2d(2, 2),
-
-            nn.Conv2d(1024, 512, kernel_size=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 1024, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(1024, 512, kernel_size=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 1024, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(1024, 1024, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(1024, 1024, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.1),
-
-            nn.Conv2d(1024, 1024, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(1024, 1024, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.1)
+            # Fully Convolutional Head
+            nn.Conv2d(1024, self.b * 5 + self.c, kernel_size=1)
         )
-
-        # Fully Convolutional Layer
-        self.head = nn.Conv2d(1024, self.b * 5 + self.c, kernel_size=1)
 
     def forward(self, x: torch.Tensor):
         """
@@ -102,7 +29,7 @@ class YOLOv1(nn.Module):
         :param x: Tensor [B, C, H, W]
         :return: Tensor permuted to [B, H, W, C]
         """
-        features = self.features(x)
-        output = self.head(features).permute(0, 2, 3, 1)  # [B, C, H, W] to [B, H, W, C]
+        features = self.backbone(x)
+        output = self.head(features).permute(0, 2, 3, 1)
 
         return output
