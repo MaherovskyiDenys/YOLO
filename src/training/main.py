1d75ffcafbba0bfd003e5c667ba1c2c82ce1f32f
Denys <maherovskyidenys@gmail.com>
2026-05-20 16:38:22 -0400

    Add freezing/unfreezing backbone during training, move vars to configs/training.py fix: optimizer now using head's params

--- src/training/main.py
+++ src/training/main.py
@@ -1,6 +1,6 @@
 import torch
 
-from configs.training import EPOCHS
+from configs.training import EPOCHS, BACKBONE_LR, BACKBONE_WEIGHT_DECAY
 from src.dataset.dataloader import get_loaders
 from src.models.model import YOLORes
 from src.training.loss import YOLOLoss
@@ -13,17 +13,29 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def train():
     model = YOLORes().to(device)
+    # Freeze backbone
+    for param in model.backbone.parameters():
+        param.requires_grad = False
 
     dataset_train, dataset_val = get_loaders()
 
     loss_func = YOLOLoss()
-    optimizer = build_optimizer(model)
+    optimizer = build_optimizer(model.head)
 
     for epoch in range(EPOCHS):
+        # Unfreeze backbone on n_th epoch
+        if epoch == 4:
+            for param in model.backbone.parameters():
+                param.requires_grad = True
+
+            optimizer.add_param_group(
+                {"params": model.backbone.parameters(), "lr": BACKBONE_LR, "weight_decay": BACKBONE_WEIGHT_DECAY}
+            )
+
         train_output = run_single_epoch_train(model, device, dataset_train, loss_func, optimizer)
         val_output = run_single_epoch_val(model, device, dataset_val, loss_func)
 
-        print(f'Train loss: {train_output:.2f} | Val loss: {val_output:.2f}')
+        print(f'#{epoch} Train loss: {train_output:.2f} | Val loss: {val_output:.2f}')
 
 
 if __name__ == "__main__":
