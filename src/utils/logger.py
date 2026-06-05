b72ca312dafad566fb0505829e537c7d3517b23f
Denys <maherovskyidenys@gmail.com>
2026-06-05 12:10:52 -0400

    Update log functions, graphs outputs val and train dataset in one graph, split only Loss, mAP

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,31 +1,47 @@
 from torch.utils.tensorboard import SummaryWriter
 from src.schema.epoch import EpochSchema
+from pathlib import Path
+from dataclasses import asdict, dataclass
 
-def log_epoch(writer: SummaryWriter, output: EpochSchema, step: int, split: str) -> None:
-    """
 
-    :param writer:
-    :param output:
-    :param step:
-    :param split:
-    :return:
-    """
-    writer.add_scalar(f"CIoU/{split}", output.ciou, global_step=step)
-    writer.add_scalar(f"Obj/{split}", output.obj, global_step=step)
-    writer.add_scalar(f"Noobj/{split}", output.noobj, global_step=step)
-    writer.add_scalar(f"Cls/{split}", output.cls, global_step=step)
-    writer.add_scalar(f"Loss/{split}", output.loss, global_step=step)
+def log_epoch(writer: SummaryWriter, output_train: EpochSchema, output_val: EpochSchema, step: int) -> None:
+    for key in asdict(output_train).keys():
+        if key != "mAP":
+            writer.add_scalars(
+                f"Loss/{key}",
+                {
+                    "train": getattr(output_train, key),
+                    "val": getattr(output_val, key)
+                },
+                global_step=step
+            )
 
-    for idx, (key, value) in enumerate(output.mAP.items()):
-        # Removes 'classes' key
-        if idx == len(output.mAP.keys()) - 1:
-           break
+    SKIP_KEYS = {
+        "classes",
+        "map_per_class",
+        "mar_100_per_class",
+    }  # some values always produce (-1.)
 
-        writer.add_scalar(f"mAP_{split}/{key}", value.item(), global_step=step)
+    for key, train_value in output_train.mAP.items():
+        if key in SKIP_KEYS:
+            continue
+
+        val_value = output_val.mAP[key]
+
+        writer.add_scalars(
+            f"mAP/{key}",
+            {
+                "train": train_value,
+                "val": val_value,
+            },
+            global_step=step
+        )
 
 def log_config(writer: SummaryWriter) -> None:
     # Read training.py and write it in tensorboard
-    with open("../../configs/training.py", "r+") as file:
-        context = file.read()
+    CONFIG_PATH = (Path(__file__).resolve().parents[2] / "configs" / "training.py")
 
-        writer.add_text("Hyperparameters", f"{context}")
+    context = CONFIG_PATH.read_text("utf-8")
+
+    writer.add_text("Hyperparameters", f"{context}")
+
