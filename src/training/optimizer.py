a2d0a304b66581fd20dd0c15af633b5b557c2bfd
Denys <maherovskyidenys@gmail.com>
2026-06-05 11:54:59 -0400

    Add a hint to an output

--- src/training/optimizer.py
+++ src/training/optimizer.py
@@ -1,5 +1,5 @@
 from torch import nn
-from torch.optim import AdamW
+from torch.optim import AdamW, Optimizer
 
 from configs.training import LR, WEIGHT_DECAY
 
@@ -8,5 +8,5 @@ def build_optimizer(
     model: nn.Module,
     lr: float = LR,
     weight_decay: float = WEIGHT_DECAY
-):
+) -> Optimizer:
     return AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
