3aad2041f588e02b191dda2e07b2482c2738b09d
Denys <maherovskyidenys@gmail.com>
2026-06-05 12:22:13 -0400

    Re-write training pipeline using utils functions

--- src/training/main.py
+++ src/training/main.py
@@ -8,6 +8,8 @@ from src.models.model import YOLORes
 from src.training.epoch import run_epoch
 from src.training.loss import YOLOLoss
 from src.training.optimizer import build_optimizer
+from src.utils.backbone import freeze_backbone, unfreeze_backbone
+from src.utils.checkpoint import Checkpoint
 from src.utils.logger import log_epoch, log_config
 from src.utils.paths import define_run_name
 
@@ -16,9 +18,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def train():
     model = YOLORes().to(device)
-    # Freeze backbone
-    for param in model.backbone.parameters():
-        param.requires_grad = False
+    freeze_backbone(model) # Freeze backbone
 
     dataset_train, dataset_val = get_loaders()
 
@@ -26,27 +26,23 @@ def train():
     optimizer = build_optimizer(model.head, lr=LR, weight_decay=WEIGHT_DECAY)
     mAP = MeanAveragePrecision(box_format="xyxy")
 
-    run_name = define_run_name(f"lr_{LR}_wd_{WEIGHT_DECAY}")
+    run_name = define_run_name()
+    checkpoint = Checkpoint()
 
-    with SummaryWriter(log_dir=run_name) as writer:
+    with SummaryWriter(run_name) as writer:
         log_config(writer)
 
         for epoch in range(EPOCHS):
-            if epoch == 5:  # Unfreeze backbone on n_th epoch
-                for param in model.backbone.parameters():
-                    param.requires_grad = True
-
-                optimizer.add_param_group(
-                    {"params": model.backbone.parameters(), "lr": BACKBONE_LR, "weight_decay": BACKBONE_WEIGHT_DECAY}
-                )
+            # Unfreeze backbone on n_th epoch
+            if epoch == 5:
+                unfreeze_backbone(model, optimizer)
 
             train_output = run_epoch(model, device, dataset_train, loss_func, mAP, optimizer)
             val_output = run_epoch(model, device, dataset_val, loss_func, mAP)
 
-            log_epoch(writer, train_output, epoch, "Train")
-            log_epoch(writer, val_output, epoch, "Val")
+            log_epoch(writer, train_output, val_output, epoch)
 
-        torch.save(model.state_dict(), f"test-output-{EPOCHS}-{LR}-{BACKBONE_LR}.pth")
+            checkpoint.update(model, val_output.mAP["map"].item(), run_name)
 
 if __name__ == "__main__":
     train()
