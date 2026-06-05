41490c6c7f96b2b6c70b126508865586ebef3440
Denys <maherovskyidenys@gmail.com>
2026-06-05 11:44:15 -0400

    Write a function to define checkpoint path, update define_run_name to take optional run_name else CURRENT_DATETIME_HOSTNAME (SummaryWriter default)

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -1,4 +1,17 @@
+import socket
+from datetime import datetime
+import os
 from pathlib import Path
+from typing import Optional
 
-def define_run_name(name: str) -> str:
-    return (Path(__file__).resolve().parents[2] / "runs" / name).as_posix()
+def define_run_name(name: Optional[str] = None) -> str:
+    base = Path(__file__).resolve().parents[2]
+
+    current_time = datetime.now().strftime("%b%d_%H-%M-%S")
+    log_dir = os.path.join(current_time + "_" + socket.gethostname())
+
+    return (base / "runs" / log_dir).as_posix() if not name else (base / "runs" / name).as_posix()
+
+
+def define_checkpoint_path(run_name: str) -> str:
+    return (Path(__file__).resolve().parents[2] / "models" / Path(run_name).name).as_posix()
