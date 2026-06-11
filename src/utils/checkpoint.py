1a235470c1723264645e48bb532b7a2e79ac62a9
Denys <maherovskyidenys@gmail.com>
2026-06-11 11:51:09 -0400

    Save optimizer state as well as model state to be able to continue training

--- src/utils/checkpoint.py
+++ src/utils/checkpoint.py
@@ -6,14 +6,19 @@ from src.utils.paths import define_checkpoint_path
 class Checkpoint:
     mAP: float = 0.0
 
-    def update(self, model, mAP, run_name):
+    def update(self, model, optimizer, mAP, run_name):
         if mAP > self.mAP:
             old_mAP = self.mAP
             self.mAP = mAP
 
             fname = define_checkpoint_path(run_name)
-            self._save_checkpoint(model, fname)
+            self._save_checkpoint(model, optimizer, fname)
             print(f"[Checkpoint Saved] mAP improved from {old_mAP:.4f} -> {self.mAP:.4f} | Filename: {fname}-mAP.pth")
 
-    def _save_checkpoint(self, model, fname):
-        torch.save(model.state_dict(), f"{fname}-mAP.pth")
+    @staticmethod
+    def _save_checkpoint(model, optimizer, fname):
+        checkpoint = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+        }
+        torch.save(checkpoint, f"{fname}-mAP.pth")
