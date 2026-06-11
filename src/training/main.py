860f0458205cf865d098019a249723f9152735d7
Denys <maherovskyidenys@gmail.com>
2026-06-11 12:17:57 -0400

    Implement visualization in training pipeline

--- src/training/main.py
+++ src/training/main.py
@@ -2,17 +2,18 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 from torchmetrics.detection import MeanAveragePrecision
 
-from configs.training import EPOCHS, LR, WEIGHT_DECAY
+from configs.config import ANCHOR_BOXES, C
+from configs.training import EPOCHS, LR, WEIGHT_DECAY, TEST_BATCH_SIZE, UNFREEZE_BACKBONE_EPOCH
 from src.dataset.dataloader import get_loaders
 from src.models.model import YOLORes
 from src.training.epoch import run_epoch
 from src.training.loss import YOLOLoss
 from src.training.optimizer import build_optimizer
-from src.utils.anchors import get_anchors
 from src.utils.backbone import freeze_backbone, unfreeze_backbone
 from src.utils.checkpoint import Checkpoint
-from src.utils.logger import log_epoch, log_config
+from src.utils.logger import log_epoch, log_config, log_images
 from src.utils.paths import define_run_name
+from src.utils.visualization import decode_images
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -21,10 +22,10 @@ def train():
     model = YOLORes().to(device)
     freeze_backbone(model) # Freeze backbone
 
-    dataset_train, _, dataloader_train, dataloader_val = get_loaders()
-    anchors = get_anchors(dataset_train.voc).to(device)
+    dataset_train, _, dataloader_train, dataloader_test = get_loaders()
+    anchors = dataset_train.anchors.to(device)
 
-    loss_func = YOLOLoss(anchors)
+    loss_func = YOLOLoss(anchors).to(device)
     optimizer = build_optimizer(model.head, lr=LR, weight_decay=WEIGHT_DECAY)
     mAP = MeanAveragePrecision(box_format="xyxy")
 
@@ -35,16 +36,35 @@ def train():
         log_config(writer)
 
         for epoch in range(EPOCHS):
-            # Unfreeze backbone on n_th epoch
-            if epoch == 5:
+            if epoch == UNFREEZE_BACKBONE_EPOCH:
                 unfreeze_backbone(model, optimizer)
 
             train_output = run_epoch(model, device, dataloader_train, loss_func, mAP, optimizer)
-            val_output = run_epoch(model, device, dataloader_val, loss_func, mAP)
+            test_output = run_epoch(model, device, dataloader_test, loss_func, mAP)
 
-            log_epoch(writer, train_output, val_output, epoch)
+            log_epoch(writer, train_output, test_output, epoch)
 
-            checkpoint.update(model, val_output.mAP["map"].item(), run_name)
+            if epoch % 2 == 0:
+                images, labels = next(iter(dataloader_test))
+                images = images.to(device)
+                labels = labels.to(device)
+
+                # Inference
+                model.eval()
+                with torch.no_grad():
+                    output = model(images[:TEST_BATCH_SIZE])
+
+                    B, S, _, _ = output.shape
+                    output = output.reshape(B, S, S, ANCHOR_BOXES, 5 + C)
+
+                    # Activate logits
+                    predicted = loss_func.activate(output)
+
+                # Decode and Log images
+                decoded = decode_images(images[:TEST_BATCH_SIZE], labels[:TEST_BATCH_SIZE], predicted)
+                log_images(writer, decoded, epoch)
+
+            checkpoint.update(model, optimizer, test_output.mAP["map"].item(), run_name)
 
 if __name__ == "__main__":
     train()
