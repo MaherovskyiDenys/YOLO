cc652f82ad45026f4fffdb91d173e75b299a8a48
Denys <maherovskyidenys@gmail.com>
2026-06-11 14:56:21 -0400

    Allow multi datasets in get_anchors function. Update comments. Change type of anchor boxes. Remove save_anchors function

--- src/utils/anchors.py
+++ src/utils/anchors.py
@@ -1,8 +1,7 @@
 import json
 from pathlib import Path
-from typing import Any
-import torch
 
+import torch
 from sklearn.cluster import KMeans
 
 from configs.config import ANCHOR_BOXES
@@ -10,74 +9,86 @@ from configs.config import ANCHOR_BOXES
 base = Path(__file__).resolve().parents[2]
 FILE_PATH = base / "configs" / "anchors.json"
 
-def get_anchors(dataset) -> torch.Tensor:
+
+def get_anchors(*datasets) -> torch.Tensor:
     """
-    Makes sure anchors.json exists and saves anchor boxes in it
-    :return:
-        Anchor boxes as a tensor with dims [N, 2]
+    Gets an anchor boxes from given datasets, saves them locally
+
+    Args:
+        *datasets: A list of given datasets
+
+    Returns:
+        Tensor: Shape [N, 2], (w,h) of anchor boxes in abs values
     """
     if FILE_PATH.exists():
-        with open(FILE_PATH, "r", encoding="utf-8") as file:
-            try:
-                anchors = json.load(file)
-            except json.JSONDecodeError:
-                anchors = None
-    else:
-        anchors = None
+        try:
+            with open(FILE_PATH, "r", encoding="utf-8") as file:
+                data = json.load(file)
+                if "anchors" in data:
+                    return torch.tensor(data["anchors"], dtype=torch.float32)
 
-    if not anchors:
-        anchors = {"anchors": identify_anchors(dataset)}
-        save_anchors(anchors)
+        except (json.JSONDecodeError, ValueError):
+            pass
 
-    return torch.tensor(anchors["anchors"]).reshape(-1, 2)
+    if not datasets:
+        raise ValueError("anchors.json not found and no datasets provided to generate them")
 
-def save_anchors(data: dict[str, Any]) -> None:
+    calculated_anchors = identify_anchors(*datasets)
+
+    # Save anchors locally
     with open(FILE_PATH, "w", encoding="utf-8") as file:
-        json.dump(data, file)
+        json.dump({"anchors": calculated_anchors}, file, indent=4)
 
-def get_wh(dataset) -> tuple[list, list]:
+    return torch.tensor(calculated_anchors, dtype=torch.float32)
+
+
+def get_wh(*datasets) -> list[list[int]]:
     """
-    Runs over given VOC 2007 dataset, outputs widths/heights of all bounding boxes
+    Collects width and heights of all the bounding boxes in given datasets
 
-    :return:
-        Raw data
-        dtype: tuple(list[widths], list[heights])
+    Args:
+        *datasets (list): A list of given datasets
+
+    Returns:
+        list[list[int]]: A list of lists with [w, h] of all the bounding boxes in abs values
     """
-    widths, heights = [], []
+    boxes = []
 
-    for _, target in dataset:
-        root = target["annotation"]
-        objects = root.get("object")
+    for dataset in datasets:
+        if dataset is None:
+            continue
 
-        # Make sure all the boxes are lists
-        if not isinstance(objects, list):
-            objects = [objects]
+        for _, target in dataset:
+            objects = target["annotation"].get("object")
 
-        for obj in objects:
-            bnd = obj["bndbox"]
-            xmin, ymin = int(bnd["xmin"]), int(bnd["ymin"])
-            xmax, ymax = int(bnd["xmax"]), int(bnd["ymax"])
+            # Make sure all the boxes are lists
+            if not isinstance(objects, list):
+                objects = [objects]
 
-            # Convert format from xyxy to cxcywh (wh only)
-            w = abs(xmax - xmin)
-            h = abs(ymax - ymin)
+            for obj in objects:
+                bnd = obj["bndbox"]
+                # Convert format from xyxy to cxcywh (wh only)
+                w = abs(int(bnd["xmax"]) - int(bnd["xmin"]))
+                h = abs(int(bnd["ymax"]) - int(bnd["ymin"]))
 
-            # Collect width and height of a box
-            widths.append(w)
-            heights.append(h)
+                # Collect width and height of a box
+                boxes.append([w, h])
 
-    return widths, heights
+    return boxes
 
-def identify_anchors(dataset):
+
+def identify_anchors(*datasets):
     """
-    Apply K-Means on width and height to identify anchor boxed
-    :return:
-        Returns list of anchor boxes with [W, H]
-    """
-    w, h = get_wh(dataset)
+    Applies K-Means on width and height to identify anchor boxes
 
-    data = list(zip(w, h))
+    Args:
+        *datasets: A list of given datasets
+
+    Returns:
+        list[list[float]]: A list with anchor boxes [w, h]
+    """
+    data = get_wh(*datasets)
     kmeans: object | KMeans = KMeans(n_clusters=ANCHOR_BOXES, random_state=0, n_init="auto").fit(data)
 
-    return kmeans.cluster_centers_.astype(int).tolist()
+    return kmeans.cluster_centers_.astype(float).tolist()
 
