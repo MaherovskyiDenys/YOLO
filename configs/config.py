6e0423485db2285a0424e7bb2e6878d2e89a5060
Denys <maherovskyidenys@gmail.com>
2026-06-11 10:45:37 -0400

    Add specific colors of a bounding box for a specific class

--- configs/config.py
+++ configs/config.py
@@ -21,6 +21,29 @@ CLASSES = [
     "tvmonitor",
 ]
 
+BBOX_COLORS = [
+    "#FF0000",
+    "#00FF00",
+    "#0000FF",
+    "#FFFF00",
+    "#FF00FF",
+    "#00FFFF",
+    "#FF6600",
+    "#FF3399",
+    "#9900FF",
+    "#008080",
+    "#CCFF00",
+    "#FF6666",
+    "#4B0082",
+    "#FFD700",
+    "#E6E6FA",
+    "#99FFCC",
+    "#DC143C",
+    "#87CEEB",
+    "#7FFF00",
+    "#FF8C00"
+]
+
 IMG_SIZE = (448, 448)  # (h, w) where h == w
 
 S = int(IMG_SIZE[0] / 32)  # reduced by factor of 32
