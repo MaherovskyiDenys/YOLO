8d3acbd8c2ce93046c124a1bf10742fcadc3e6b6
Denys <maherovskyidenys@gmail.com>
2026-06-14 14:01:50 -0400

    Update step size

--- src/training/scheduler.py
+++ src/training/scheduler.py
@@ -1,4 +1,4 @@
 from torch import optim
 
-def build_scheduler(optimizer: optim.Optimizer, step_size=2, gamma=0.1):
+def build_scheduler(optimizer: optim.Optimizer, step_size=5, gamma=0.1):
     return optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=step_size, gamma=gamma)
