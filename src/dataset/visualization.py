d37e2cbdbf1c75164bb62c9e8527b1ef197f0f46
Denys <maherovskyidenys@gmail.com>
2026-06-14 12:15:38 -0400

    Update docstring

--- src/dataset/visualization.py
+++ src/dataset/visualization.py
@@ -2,6 +2,16 @@ import matplotlib.pyplot as plt
 
 
 def visualize_width_height(w, h):
+    """
+    Visualizes width and height distribution in a dataset
+
+    Args:
+        w (list): Widths
+        h (list): Heights
+
+    Returns:
+        Plots W vs H distribution, returns nothing
+    """
     plt.scatter(w, h, alpha=0.3, color='blue', edgecolors='none', s=10)
 
     plt.title('Bounding Box Width vs Height Distribution')
