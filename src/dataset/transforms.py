1b6ed96a2ee52326ff477ccd3871aa83ae13168c
Denys <maherovskyidenys@gmail.com>
2026-06-14 12:00:40 -0400

    Apply data augmentation from YOLO v2 paper

--- src/dataset/transforms.py
+++ src/dataset/transforms.py
@@ -5,7 +5,12 @@ from configs.config import IMG_SIZE
 
 train_transform = v2.Compose([
     v2.ToImage(),
+    v2.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1), interpolation=v2.InterpolationMode.BILINEAR),
+    v2.SanitizeBoundingBoxes(min_area=16.0),
+    v2.RandomHorizontalFlip(p=0.5),
+    v2.ColorJitter(brightness=0.5, contrast=0.0, saturation=0.5, hue=0.1),
     v2.Resize(IMG_SIZE),
+
     v2.ToDtype(torch.float32, scale=True),
     v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
@@ -13,6 +18,7 @@ train_transform = v2.Compose([
 val_transform = v2.Compose([
     v2.ToImage(),
     v2.Resize(IMG_SIZE),
+
     v2.ToDtype(torch.float32, scale=True),
     v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
