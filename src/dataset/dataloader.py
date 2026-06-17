35e64dde4e44fb9d5820e0b0326367200dff025a
Denys <maherovskyidenys@gmail.com>
2026-06-17 18:01:48 -0400

    Update variable name

--- src/dataset/dataloader.py
+++ src/dataset/dataloader.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 
 from configs.training import BATCH_SIZE, NUM_WORKERS
 from src.dataset.dataset import VOCDatasetYOLO
-from src.dataset.transforms import train_transform, val_transform
+from src.dataset.transforms import train_transform, test_transform
 
 base = Path(__file__).resolve().parents[2]
 
@@ -17,7 +17,7 @@ def get_loaders(
     pin_memory: bool = torch.cuda.is_available(),
 ):
     train_dataset = VOCDatasetYOLO(base / root, image_set="trainval", transforms=train_transform)
-    test_dataset = VOCDatasetYOLO(base / root, image_set="test", transforms=val_transform)
+    test_dataset = VOCDatasetYOLO(base / root, image_set="test", transforms=test_transform)
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
