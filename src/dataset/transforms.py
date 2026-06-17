6159fc462a08ea617c07ba89ef2f773d8d6a76f3
Denys <maherovskyidenys@gmail.com>
2026-06-17 18:02:06 -0400

    Update variable name

--- src/dataset/transforms.py
+++ src/dataset/transforms.py
@@ -15,7 +15,7 @@ train_transform = v2.Compose([
     v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-val_transform = v2.Compose([
+test_transform = v2.Compose([
     v2.ToImage(),
     v2.Resize(IMG_SIZE),
 
